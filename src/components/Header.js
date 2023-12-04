30b748cb37d830b997452dfe9e3138b67ed94853
Hemxnt <hemant154csec21@bpitindia.edu.in>
2023-12-04 21:11:12 +0530

    header logo and Sign In / Sign Up Form

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='absolute px-8 py-4 bg-gradient-to-b from-black z-10'>        
+        <img 
+        className='w-44'
+        src='https://youareawful.com/img/streamberry.a33c452d.svg'
+        alt='logo'
+        />
+    </div>
+  )
+}
+
+export default Header;
